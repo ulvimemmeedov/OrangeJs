854bae22c2f72523790214df16fc0750a2c20547
Ulvi Memmedov <onezerolord@gmail.com>
2022-01-31 23:09:22 +0400

    fix v8 include

--- src/Console/Console.hpp
+++ src/Console/Console.hpp
@@ -3,8 +3,19 @@
 #define CONSOLE
 using namespace v8;
 
-namespace Console 
+namespace Console
 {
-        static void WriteLine(const FunctionCallbackInfo<Value> &args);	
+        static void WriteLine(const FunctionCallbackInfo<Value> &args)
+        {
+                HandleScope scope(args.GetIsolate());
+
+                String::Utf8Value str(args.GetIsolate(), args[0]);
+                const char *cstr = StaticHelpers::ToCString(str);
+
+                fprintf(stdout, "%s", cstr);
+
+                fprintf(stdout, "\n");
+                fflush(stdout);
+        }
 }
 #endif
